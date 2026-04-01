d6eb83bfff39ff2a082038f3a89d9e9d08a6c3a7
rafiqul1119750-gif <rafiqul1119750@gmail.com>
2026-04-02 01:57:25 +0530

    Add shared folder to Tailwind CSS content paths

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "../shared/**/*.{js,ts,jsx,tsx}" // Agar shared folder use kar rahe ho toh
   ],
   theme: {
     extend: {},
