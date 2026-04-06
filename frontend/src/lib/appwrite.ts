301966a13336ffbe065f431e6d71d1326a25cb6d
rafiqul1119750-gif <rafiqul1119750@gmail.com>
2026-04-06 21:16:09 +0530

    Update appwrite.ts

--- frontend/src/lib/appwrite.ts
+++ frontend/src/lib/appwrite.ts
@@ -1,10 +1,14 @@
-import { Client, Account, Databases, Storage } from 'appwrite';
+import { Client, Account, Databases, Storage, Avatars } from 'appwrite';
 
-export const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') 
-    .setProject('69b9929d0024fe351bc2'); // <--- Yahan apni Project ID dalo
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1') // Standard Appwrite Cloud Endpoint
+    .setProject('69b9929d0024fe351bc2'); // 👈 Yahan apni Appwrite Project ID dalein
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
-export { ID } from 'appwrite';
+export const avatars = new Avatars(client);
+
+export default client;
