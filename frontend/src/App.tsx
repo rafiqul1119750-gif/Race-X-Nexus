2fd10a8a83ad3e705f25054b7ffcbf32d9ecf8b2
rafiqul1119750-gif <rafiqul1119750@gmail.com>
2026-04-08 19:19:33 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,13 @@ import { queryClient } from "./lib/queryClient";
 import { Toaster } from "./components/ui/toaster";
 import { AppProvider } from "./context/AppContext";
 
+// --- CORE SCREENS (Preserved) ---
 import SplashScreen from "./pages/splash";
+import MainHub from "./pages/hub";
 import SignIn from "./pages/Auth/signin";
 import SignUp from "./pages/Auth/signup";
-import MainHub from "./pages/hub";
-import RXStudio from "./pages/studio/index"; 
 
+// --- SOCIAL MODULES (Connecting Dead Icons) ---
 import SocialFeed from "./pages/social/feed";
 import SearchPage from "./pages/social/search";
 import CommentsPage from "./pages/social/comments";
@@ -18,26 +19,40 @@ import ExplorePage from "./pages/social/explore";
 import ActivityPage from "./pages/social/activity";
 import CreatePost from "./pages/social/create";
 
+// --- STUDIO ---
+import RXStudio from "./pages/studio/index";
+
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AppProvider> 
-        <Switch>
-          <Route path="/" component={SplashScreen} />
-          <Route path="/auth/signin" component={SignIn} />
-          <Route path="/auth/signup" component={SignUp} />
-          <Route path="/hub" component={MainHub} />
-          <Route path="/social/feed" component={SocialFeed} />
-          <Route path="/social/explore" component={ExplorePage} />
-          <Route path="/social/search" component={SearchPage} />
-          <Route path="/social/activity" component={ActivityPage} />
-          <Route path="/social/create" component={CreatePost} />
-          <Route path="/social/comments/:id" component={CommentsPage} />
-          <Route path="/profile" component={UserProfile} /> 
-          <Route path="/studio" component={RXStudio} />
-          <Route><Redirect to="/hub" /></Route>
-        </Switch>
-        <Toaster />
+        <div className="min-h-screen bg-black text-white">
+          <Switch>
+            {/* 1. Splash Screen Pehle Ayegi */}
+            <Route path="/" component={SplashScreen} />
+            
+            {/* 2. Auth & Main Hub */}
+            <Route path="/auth/signin" component={SignIn} />
+            <Route path="/auth/signup" component={SignUp} />
+            <Route path="/hub" component={MainHub} />
+            
+            {/* 3. Social Engine (Ab Saare Dead Icons Live Honge) */}
+            <Route path="/social/feed" component={SocialFeed} />
+            <Route path="/social/explore" component={ExplorePage} />
+            <Route path="/social/search" component={SearchPage} />
+            <Route path="/social/activity" component={ActivityPage} />
+            <Route path="/social/create" component={CreatePost} />
+            <Route path="/social/comments/:id" component={CommentsPage} />
+            <Route path="/profile" component={UserProfile} /> 
+            
+            {/* 4. Studio Module */}
+            <Route path="/studio" component={RXStudio} />
+
+            {/* Default Redirect */}
+            <Route><Redirect to="/hub" /></Route>
+          </Switch>
+          <Toaster />
+        </div>
       </AppProvider>
     </QueryClientProvider>
   );
