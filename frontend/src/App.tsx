4838cfe021caa4ed19c960727be395068222a381
rafiqul1119750-gif <rafiqul1119750@gmail.com>
2026-04-08 15:00:03 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { Switch, Route, Redirect } from "wouter";
+Appcontexttexttext Switch, Route, Redirect } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "./components/ui/toaster";
 
 // SIRF YE WALA CONTEXT USE HOGA (Diamonds + Theme dono isi mein hain)
-import { AppProvider } from "./connect/Appcontext";
+import { AppProvider } from "./connect/AppContext";
 
 // CORE PAGES
 import SplashScreen from "./pages/splash";
