d392f98a3520fdc542aa46ff6b381c7733fba2d2
rafiqul1119750-gif <rafiqul1119750@gmail.com>
2026-04-12 18:02:52 +0530

    Update index.tsx

--- frontend/src/pages/studio/index.tsx
+++ frontend/src/pages/studio/index.tsx
@@ -7,22 +7,20 @@ import Analytics from "./analytics";
 
 export default function StudioIndex() {
   return (
-    <div className="min-h-screen bg-black overflow-x-hidden">
-      <Switch>
-        {/* /studio pe Dashboard dikhega */}
-        <Route path="/studio" component={Dashboard} />
-        
-        {/* Full Power Features */}
-        <Route path="/studio/editor" component={Editor} />
-        <Route path="/studio/video" component={VideoGen} />
-        <Route path="/studio/voice" component={VoiceLab} />
-        <Route path="/studio/analytics" component={Analytics} />
+    <Switch>
+      {/* Base Path */}
+      <Route path="/studio" component={Dashboard} />
+      
+      {/* Sub-paths (Yahan se click hoke jayega) */}
+      <Route path="/studio/editor" component={Editor} />
+      <Route path="/studio/video" component={VideoGen} />
+      <Route path="/studio/voice" component={VoiceLab} />
+      <Route path="/studio/analytics" component={Analytics} />
 
-        {/* Galat path pe wapas Home */}
-        <Route>
-          <Redirect to="/studio" />
-        </Route>
-      </Switch>
-    </div>
+      {/* Agar kuch galat ho toh wapas Dashboard */}
+      <Route>
+        <Redirect to="/studio" />
+      </Route>
+    </Switch>
   );
 }
